bf8a0a27865b623116beb9a599f1411ee6d3b12b
CleoCyber21 <63235670+CleoCyber21@users.noreply.github.com>
2020-04-17 00:39:57 +0700

    Create upnow.sh

---
+++ upnow.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+clear
+
+blue='\033[34;1m'
+green='\033[32;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By Kak tutorial
+echo "'\033[33;1m'SELAMAT DATANG DI TOOLS KAK TUTORIAL"
+echo ""
+echo ""
+sleep 1
+echo "'\033[32;1m'JANGAN LUPA SUBSCRIBE CHANNEL KAK TUTORIAL"
+echo ""
+sleep 1
+echo "'\033[34;1m'INI TOOLS CEPATKAN JARINGAN"
+sleep 2
+clear
+
+
+echo "'\033[32;1m'SOBAT PILIH YA"
+echo ""
+sleep 1
+echo "_-_-_-_-_-_-_-_-"
+echo ""
+sleep 1
+echo "    (1) LOGIN "
+echo "    (2) DM INSTAGRAM"
+echo "    (3) EXIT "
+read -p "SILAKAN PILIH SOBAT! : " nomor
+if [ $nomor = "1" ]
+then
+
+clear
+echo "'\033[32;1m'USERNAME DAN PASSWORD JANGAN DI PUBLIKASIKAN!"
+echo "SILAKAN TULIS DISINI "
+echo " username = CLEO , password = CYBER "
+sleep 1
+nama="CLEO"
+while [ "$siapa" != "$nama" ]
+do
+read -p "Apa username nya : " siapa
+done
+nama="CYBER"
+while [ "$siapa" != "$nama" ]
+do
+read -p "Apa pass nya : " siapa
+done
+
+clear
+
+elif [ $nomor = "2" ]
+then
+sleep 2
+echo "tanya aja"
+echo "official instagram"
+echo " cleocyber_ "
+sleep 4
+clear
+elif [ $nomor = "3" ]
+then
+toilet -f big -F gay EXIT
+exit
+fi
+
+ulang="y"
+while [ $ulang = "y" ];
+do
+
+echo "'\033[35;1m'HALLO SOBAT CYBER"
+sleep 1
+echo "TINGGAL PILIH YA SESUAI KEBUTUHAN"
+echo "__________________________________"
+echo ""
+sleep 1
+echo "(1) MULAI"
+echo "(2) UPDATE Tools INSTAGRAM"
+echo "(3) EXIT"
+read -p "PILIH NOMOR DI ATAS INI : " pilih
+if [ $pilih = "1" ]
+then
+clear
+toilet -f big -F gay CLEO CYBER
+echo $green "Cepatkan Jaringan By CLEO CYBER"
+ping -s9000 1.1.1.1
+
+elif [ $pilih = "2" ]
+then
+echo "sabar"
+echo "https://www.instagram.com/cleocyber_/"
+echo "salin tuh"
+echo "udah kan"
+sleep 2
+elif [ $pilih = "2" ]
+then
+toilet -f big -F gay EXIT
+exit
+
+else
+echo "ERROR ATAU ANDA SALAH MENGENTIKAN ANGKA"
+echo "mohon ulang"
+echo $ulang
+sleep 2
+fi
+done
+
